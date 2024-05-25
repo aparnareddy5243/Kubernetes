3049a7d1d8380792dcd85e7aa85b14c002aa2174
appureddyt <apputmrreddy@gmail.com>
2024-05-25 19:23:13 +0100

    gg1

--- gweb/Kubcluster_files/main.tf
+++ gweb/Kubcluster_files/main.tf
@@ -10,7 +10,7 @@ terraform {
     resource_group_name   = "ap_rg1"
     storage_account_name  = "terraform524"
     container_name        = "kubecontainer"
-    key                   = "terraform.tfstate"
+    # No key specified here
   }
 }
 
